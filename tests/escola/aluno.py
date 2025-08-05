fe49a9d93dcce53898be801f8648f5c0fe05a57b
Gabriela <severogabriela4@gmail.com>
2025-08-05 11:32:10 -0300

    teste com notas

--- tests/escola/aluno.py
+++ tests/escola/aluno.py
@@ -1,3 +1,9 @@
 def calcular_media(entrada:list[float]) -> float:
+    if not isinstance(entrada, list[float]):
+        raise ValueError("As notas não é uma lista")
+
+    if len(entrada) == 0:
+        raise ValueError("Não é permitido uma lista vazia")
+
     media = sum(entrada)/len(entrada)
-    return media
+    return round(media,1)
