10e0de17e95f5be0c81428600561ab8630aa6f27
Gabriela <severogabriela4@gmail.com>
2025-08-12 11:00:46 -0300

    correção nas funções

--- tests/escola/aluno.py
+++ tests/escola/aluno.py
@@ -1,9 +1,28 @@
 def calcular_media(entrada:list[float]) -> float:
-    if not isinstance(entrada, list[float]):
-        raise ValueError("As notas não é uma lista")
 
+    """"
+    Calcula a média de uma lista de notas. 
+
+    Parâmetros:
+    notas (list[float]): Lista de notas a serem calculadas.
+
+    Retorna:
+    float: A média das notas, arredondada para uma casa decimal.
+    """
+
+    #Validando se entrada é uma lista 
+    if not isinstance(entrada, list):
+        raise TypeError("Nota invalida")
+
+    #Validando se a lista é vazia
     if len(entrada) == 0:
         raise ValueError("Não é permitido uma lista vazia")
+
+    for entradas in entrada:
+        if not type(entradas) == int and not type(entradas) == float:
+            raise TypeError("nota invalida")
+        if entradas < 0 or entradas > 10:
+            raise ValueError("nota invalida")
 
     media = sum(entrada)/len(entrada)
     return round(media,1)
--- tests/test_calcular_media_excecoes.py
+++ tests/test_calcular_media_excecoes.py
@@ -8,3 +8,28 @@ def test_calcular_media_lista_vazia():
     #Executando a função e esperando erro 
     with pytest.raises(ValueError, match="não é permitido uma lista vazia"):
         calcular_media(entrada)
+
+
+def test_calcular_media_enviando_string():
+    #Definindo a entrada
+    entrada = "ola"
+
+    #Executando a função e esperando erro 
+    with pytest.raises(TypeError, match="nota invalida"):
+        calcular_media(entrada)
+
+def test_calcular_media_enviando_string_como_nota():
+    #Definindo a entrada
+    entrada = [(3.0), 2.0]
+
+    #Executando a função e esperando erro 
+    with pytest.raises(TypeError, match="nota invalida"):
+        calcular_media(entrada)
+
+def test_calcular_media_com_numero_negativo():
+    #Definindo a entrada
+    entrada = [-10.0]
+
+    #Executando a função e esperando erro 
+    with pytest.raises(ValueError, match="limite da nota [0, 10 ]"):
+        calcular_media(entrada)
